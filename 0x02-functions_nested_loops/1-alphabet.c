cd5d252c147f07f622cc8aa2b7fc41f33273a416
Aamrou <darhmaouiaamr@gmail.com>
2023-07-18 11:34:45 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,7 @@
 
 /**
  * print_alphabet - Entry point
- *
- * Return: Always  0.
+ * Return:0
  */
 
 void print_alphabet(void)
@@ -18,5 +17,5 @@ void print_alphabet(void)
 
 	}
 	_putchar('\n');
-	return (0);
+
 }
