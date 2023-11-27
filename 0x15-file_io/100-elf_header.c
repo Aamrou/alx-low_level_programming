2afa5b74a7e7416ad9f4526b6e74f5b3cbc4df98
Aamrou <darhmaouiaamr@gmail.com>
2023-11-27 06:59:49 -0800

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -112,15 +112,15 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- *  * print_version - Prints the version of an ELF header.
- *   * @e_ident: A pointer to an array containing the ELF version.
- *    */
+ *  print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
+ */
 void print_version(unsigned char *e_ident)
 {
 	printf(" Version: %d",
 			 e_ident[EI_VERSION]);
 
-	 switch (e_ident[EI_VERSION])
+	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
 		printf(" (current)\n");
