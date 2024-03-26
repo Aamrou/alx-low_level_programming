940ff8f1f89bbc9bc859ee6a57a67f0251cf2e47
Aamrou <darhmaouiaamr@gmail.com>
2024-03-26 10:44:53 -0700

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
-""" This module computes the perimeter of an island"""
+""" returns the perimeter of the island described in grid
+
+- grid is a list of list of integers:
+
+    * 0 represents a water zone
+    * 1 represents a land zone
+    * One cell is a square with side length 1
+    * Grid cells are connected horizontally/vertically (not diagonally).
+    * Grid is rectangular, width and height don’t exceed 100
+
+- Grid is completely surrounded by water, and there is one island (or nothing).
+
+- The island doesn’t have “lakes” (water inside that isn’t connected to the
+    water around the island).
+"""
 
 
 def island_perimeter(grid):
-    """ island_perimeter:
-                    Args: grid, a list of list of integers
-                    Return: perimeter of the island described in grid
-    """
-
     perimeter = 0
-    env_list = []
-
-    for items in range(len(grid)):
-        if items == 0 or items == len(grid):
-            continue
-        for cell in range(len(grid[items])):
-            if cell == 0 or cell == len(grid[items]):
-                continue
-            else:
-                if grid[items][cell] == 1:
-                    env_list = [grid[items - 1][cell], grid[items][cell + 1],
-                                grid[items + 1][cell],  grid[items][cell - 1]]
-                    count = 0
-                    for j in env_list:
-                        if j == 0:
-                            count = count + 1
-                    if (count == 4):
-                        return 4
-                    elif (count == 3):
-                        perimeter = perimeter + 3
-                    elif (count == 2):
-                        perimeter = perimeter + 2
-                    elif (count == 1):
-                        perimeter = perimeter + 1
-                    else:
-                        pass
-                    env_list = []
-                else:
-                    pass
+    rows = len(grid)
+    columns = len(grid[0])
+    for row in range(rows):
+        for column in range(columns):
+            # only if Cell has value(land)
+            if grid[row][column]:
+                # if it's first row or previous cell in that row is water(0)
+                if (row - 1 < 0 or grid[row - 1][column] == 0):
+                    perimeter += 1
+                # if it's last row or next cell in that row is water(0)
+                if (row + 1 >= rows or grid[row + 1][column] == 0):
+                    perimeter += 1
+                # if first column or previous cell in that column is water(0)
+                if (column - 1 < 0 or grid[row][column - 1] == 0):
+                    perimeter += 1
+                # if last column or next cell in that column is water(0)
+                if (column + 1 >= columns or grid[row][column + 1] == 0):
+                    perimeter += 1
     return perimeter
 
